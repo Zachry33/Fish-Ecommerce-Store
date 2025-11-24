39a72e3fc050758b074b320db21fbede22ab4a1a
Zach Simpson <Zach33Simpson@gmail.com>
2025-11-24 16:54:08 -0500

    Fixed alignment of featured products

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-  
+    document.getElementById('search-button').
+                addEventListener('click', function () {
+                   // Add search page
+                   console.log("hi");
+                });
 });
