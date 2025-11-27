4f10129b9436ba58d94f24685ebb8acbda3e6e11
DrDizzy34 <logan.reyes034@gmail.com>
2025-11-26 19:58:38 -0500

    added rest of original fish

--- server/makedb.js
+++ server/makedb.js
@@ -14,11 +14,12 @@ for (let file of fs.readdirSync('./server/gen')) {
     await client.query(sql_string);
 }
 
-add_product("goldfish.jpg", "Goldfish", "silly little goldfishies", 7, 9);
-add_product("guppy.jpg", "Guppy", "Bubble Guppies???", 6, 16);
-add_product("molly.jpg", "Molly", "Molly long, go B", 5, 6);
-add_product("Neon-Tetra-1.jpg", "Neon Tetra", "i don't have a joke for this", 4, 12);
-
+add_product("goldfish.jpg", "Goldfish", "Small freshwater carp. Average houshold pet.", 3, 30);
+add_product("guppy.jpg", "Guppy", "Popular South American freshwater fish", 6, 18);
+add_product("molly.jpg", "Molly", "Small oblong fish with wide range of fin shapes.", 5, 25);
+add_product("Neon-Tetra-1.jpg", "Neon Tetra", "Vibrant colorful school fish.", 4, 12);
+add_product("betta.jpg", "Betta Fish", "Siamese fighting fish", 10, 15);
+add_product("pleco.jpg", "Plecostomus", "Algae eating freshwater fish.", 8, 5);
 add_user("testuser123", "testemail123@gmail.com", "password123");
 add_user("admin", "adminemailidk@gmail.com", "password321");
 console.log("DONE!");
